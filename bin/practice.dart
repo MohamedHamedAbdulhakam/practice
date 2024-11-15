ac71b6032311e6bf77df5705957170b62f1a6e7d
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-11-15 18:50:18 +0200

    mixins

--- bin/practice.dart
+++ bin/practice.dart
@@ -2,4 +2,22 @@ import 'package:practice/practice.dart' as practice;
 
 void main(List<String> arguments) {
   print('Hello world');
+  ;
+  Snake mesho = Snake();
+  mesho.
 }
+
+class Animal {
+  int numberOfLimb = 2;
+  walk() {}
+}
+
+mixin Reptiles {
+  crawl() {}
+}
+
+class dog extends Animal {}
+
+class Cat extends Animal {}
+
+class Snake extends Animal with Reptiles {}
